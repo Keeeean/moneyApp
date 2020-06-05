9a17efa3f307b915f37e1ea3f76c787316fcfd03
Keeeean <suk@macstudent.com.au>
2020-06-05 14:30:56 +1000

    yes

--- salesTax.py
+++ salesTax.py
@@ -1,31 +1,21 @@
 taxRates = open("taxRates")
 dic = {}
 for line in taxRates:
-    line = line.strip().split()
+    line = line.strip().split(',')
     abbrev_state = line[0]
     rates = line[1]
-    dic[abbrev_state] = float(rates)
-
-
-
-
-
-
-
+    dic[abbrev_state] = rates
 
 def error_message():
 
     print("Something went wrong!")
 
-
 def calculate_sales_tax(chosenState, price):
     if chosenState in dic:
-        salePrice = price + ((dic[chosenState]/100) * price)
-
-    return salePrice
-
-
+        chosen_rate = dic[chosenState]
+        price = ((float(chosen_rate)/100)*price)+price
+    return price
 
 
 
-print(dic)
+
